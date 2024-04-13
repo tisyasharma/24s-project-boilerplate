b02b2df565c516e3c529ae30bb09c2009b2b652e
Spring23-TCF <mccoy.jas@northeastern.edu>
2024-04-12 20:52:17 -0400

    routes folder

---
+++ flask-app/src/routes/users.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+
+users = Blueprint('users', __name__)
+
+# Get customer detail for customer with particular userID
+@users.route('/users/<userID>', methods=['GET'])
+def get_user(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from customers where id = {0}'.format(userID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
